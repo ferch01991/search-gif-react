0a143335e783a04b8d44cdb57bccf85f128869b9
Fernando Herrera <faherrera2@gmail.com>
2020-11-26 22:07:08 -0500

    waitForNextUpdate

--- src/test/hooks/useFetchGifs.test.js
+++ src/test/hooks/useFetchGifs.test.js
@@ -4,15 +4,28 @@ const { useFetchGifs } = require("../../hooks/useFetchGifs")
 
 describe('Pruebas para el custom hooks useFetchGifs', () => {
     
-    test('should return initial state', () => {
+    test('should return initial state', async () => {
         
-        const { result } = renderHook( () => useFetchGifs( 'One Punch ') )
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'One Punch ') )
         
         const { data, loading } = result.current;
+        await waitForNextUpdate()
         // console.log(data, loading)
 
         expect( data ).toEqual( [] )
         expect( loading ).toBe( true )
+    });
+
+    test('should return images array and true value loading', async () => {
+        
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'One Punch ') )
+        await waitForNextUpdate()
+
+        const { data, loading } = result.current;
+        // console.log(data, loading)
+
+        expect( data.length ).toBe( 10 )
+        expect( loading ).toBe( false )
     })
     
 })
